25fa68f5aab8a978bf18eefb3734693f48abd9d5
otogelashvili <otarbasilevsi@gmail.com>
2026-04-04 03:55:17 +0400

    feat: add third step UI

--- src/app/shared/components/auth/sign-up/sign-up.ts
+++ src/app/shared/components/auth/sign-up/sign-up.ts
@@ -12,23 +12,31 @@ export class SignUp {
   closeModal = output<void>();
   passwordVisible = signal(true);
   confrimPasswordVisible = signal(false);
+  selectedFile = signal<File | null>(null);
+  previewUrl = signal<string | null>(null);
+  isDragging = signal(false);
+  steps = signal(1);
+
   signUpForm = new FormGroup({
     email: new FormControl<string>('you@example.com', [Validators.required, Validators.email]),
     username: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
     password: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
     confirmPassword: new FormControl<string>('', [Validators.required]),
-    avatar: new FormControl('', [Validators.pattern(/\.(jpg|jpeg|png|webp)$/i)]),
+    avatar: new FormControl(''),
   });
 
-  steps = signal(1);
-
+  //close and reset modal
   onClose() {
     this.steps.set(1);
     this.closeModal.emit();
   }
+
+  //back arrow
   decreaseStep() {
     this.steps.update((v) => v - 1);
   }
+
+  //password display
   togglePassword() {
     this.passwordVisible.update((v) => !v);
   }
@@ -36,6 +44,7 @@ export class SignUp {
     this.confrimPasswordVisible.update((v) => !v);
   }
 
+  //btn disabled logic
   getIsBtnDisabled(): boolean {
     const step = this.steps();
     const form = this.signUpForm;
@@ -57,6 +66,58 @@ export class SignUp {
 
     return form.invalid;
   }
+
+  //avatar
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.setFile(file);
+  }
+
+  private setFile(file: File) {
+    if (!file) return;
+
+    const control = this.signUpForm.get('avatar');
+    const maxSize = 2 * 1024 * 1024;
+    const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
+
+    if (!allowedTypes.includes(file.type)) {
+      control?.setErrors({ incorrectFormat: true });
+      return;
+    }
+    if (file.size > maxSize) {
+      control?.setErrors({ tooLarge: true });
+      return;
+    }
+    control?.setErrors(null);
+
+    this.selectedFile.set(file);
+    this.previewUrl.set(URL.createObjectURL(file));
+    control?.setValue(file.name);
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragging.set(true);
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragging.set(false);
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragging.set(false);
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      this.setFile(files[0]);
+    }
+  }
+  //submit
   onSubmit() {
     if (this.steps() === 3) {
       console.log('submit');
