516ec60fb1156d8c083fc1b464679c4e6574883c
otogelashvili <otarbasilevsi@gmail.com>
2026-04-03 20:32:29 +0400

    feat: make first step of signUp form functional

--- src/app/shared/components/auth/sign-up/sign-up.ts
+++ src/app/shared/components/auth/sign-up/sign-up.ts
@@ -1,15 +1,33 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
 export class SignUp {
   closeModal = output<void>();
+  signUpForm = new FormGroup({
+    email: new FormControl<string>('you@example.com', [Validators.required, Validators.email]),
+    username: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
+    confirmPassword: new FormControl<string>('', [Validators.required]),
+    avatar: new FormControl('', [Validators.pattern(/\.(jpg|jpeg|png|webp)$/i)]),
+  });
+
+  steps = signal(1);
 
   onClose() {
+    this.steps.set(1);
     this.closeModal.emit();
+  }
+  onSubmit() {
+    if (this.steps() === 3) {
+      console.log('submit');
+    } else {
+      this.steps.update((v) => v + 1);
+    }
   }
 }
