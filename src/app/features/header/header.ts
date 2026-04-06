a3b7033c33b5f3ddc7bb36c88442d62b244b361b
otogelashvili <otarbasilevsi@gmail.com>
2026-04-06 08:49:37 +0400

    feat: add confirmation of closing incomplete profile modal

--- src/app/features/header/header.ts
+++ src/app/features/header/header.ts
@@ -5,6 +5,8 @@ import { AuthService } from '../../core/services/auth.service';
 import { LogIn } from '../auth/log-in/log-in';
 import { SignUp } from '../auth/sign-up/sign-up';
 import { Profile } from '../profile/profile';
+import { NotificationService } from '../../core/services/notification.service';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -17,19 +19,35 @@ export class Header {
   protected isLogInModalRendered = signal(false);
   protected isProfileModalRendered = signal(false);
   protected readonly authService = inject(AuthService);
-  toggleModal() {
+  protected readonly notyService = inject(NotificationService);
+  protected toggleModal() {
     this.isLogInModalRendered.set(false);
     this.isProfileModalRendered.set(false);
     this.isModalRendered.update((v) => !v);
   }
-  toggleLogInModal() {
+  protected toggleLogInModal() {
     this.isModalRendered.set(false);
     this.isProfileModalRendered.set(false);
     this.isLogInModalRendered.update((v) => !v);
   }
-  toggleProfileModal() {
+  protected async toggleProfileModal() {
+    if (!this.authService.user()?.profileComplete && this.isProfileModalRendered()) {
+      const confirmed = await this.notyService.confirm(
+        "Your profile is incomplete. You won't be able to enroll in courses until you complete it. Close anyway?",
+      );
+
+      if (!confirmed) {
+        return;
+      }
+    }
+
     this.isLogInModalRendered.set(false);
     this.isModalRendered.set(false);
     this.isProfileModalRendered.update((v) => !v);
+  }
+  @HostListener('document:keydown.escape') async onEscape() {
+    if (this.isProfileModalRendered()) {
+      await this.toggleProfileModal();
+    }
   }
 }
