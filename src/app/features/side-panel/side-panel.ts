6b59fd9039e9f03886ff9db49ead266c51d7f41a
otogelashvili <otarbasilevsi@gmail.com>
2026-04-09 09:26:49 +0400

    feat: add empty state side panel

--- src/app/features/side-panel/side-panel.ts
+++ src/app/features/side-panel/side-panel.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CoursesService } from '../../core/services/courses.service';
+import { EnrolledCourse } from '../../models/courses.model';
+import { NotificationService } from '../../core/services/notification.service';
 
 @Component({
   selector: 'app-side-panel',
@@ -6,4 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './side-panel.html',
   styleUrl: './side-panel.css',
 })
-export class SidePanel {}
+export class SidePanel implements OnInit {
+  private readonly courseService = inject(CoursesService);
+  private readonly notyService = inject(NotificationService);
+  protected isLoadingCourses = signal(false);
+  protected enrolledCourses = signal<EnrolledCourse[]>([]);
+
+  ngOnInit() {
+    this.loadCourses();
+  }
+
+  private async loadCourses() {
+    this.isLoadingCourses.set(true);
+    try {
+      const courses = await this.courseService.getEnrolledCourses();
+      const inProgress = courses.filter((course) => course.progress < 100);
+      this.enrolledCourses.set(inProgress);
+    } catch {
+      this.notyService.showError('Failed to load featured courses');
+    } finally {
+      this.isLoadingCourses.set(false);
+    }
+  }
+}
